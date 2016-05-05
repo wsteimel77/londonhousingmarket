1d854b0e3140bd8afd15573d374c43c7c31e3611
Weston Steimel <wsteimel77@gmail.com>
2016-05-05 00:29:53 -0500

    Make port a parameter to go server app

--- server.go
+++ server.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+var port int
+
+func init() {
+	flag.IntVar(&port, "p", 3000, "Port")
+	flag.Parse()
+}
+
 func main() {
 	var dir = http.Dir("site")
-  http.Handle("/", http.FileServer(dir))
-  log.Fatal(http.ListenAndServe(":3000", nil))
+	http.Handle("/", http.FileServer(dir))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
